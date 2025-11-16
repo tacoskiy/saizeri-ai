6345a7b8c301da73f00fcc93ce549135ed906b0d
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-11-17 01:13:03 +0900

    fix ResultBox

--- frontend/src/components/ResultsBox.tsx
+++ frontend/src/components/ResultsBox.tsx
@@ -10,19 +10,22 @@ const Menu =[
     {
     image:"/menuImg.png",
     name:"ミラノ風ドリア",
-    price:"273円(税込300円)",
+    price:"273円",
+    price2:"(税込305円)",
     pass:"2101"
     },
     {
     image: "/menuImg.png",
     name: "海老グラタン",
-    price: "320円(税込352円)",
+    price: "320円",
+    price2:"(税込352円)",
     pass: "2102",
   },
   {
     image: "/menuImg.png",
     name: "チーズリゾット",
-    price: "280円(税込308円)",
+    price: "280円",
+    price2:"(税込312円)",
     pass: "2103",
   },
 ]
@@ -32,7 +35,7 @@ export function ResultsBox() {
         <div className="flex flex-col items-center justify-start gap-5 p-3">
             <p className="text-3xl font-bold text-green">AIからの提案</p>
             {Menu.map((Menu, index) => (
-                <div key={index} className="flex justify-start w-[452px] h-45 bg-linear-to-r from-[#ffffff33] to-[#3435411A] border-2 border-green rounded-3xl p-4 gap-4">
+                <div key={index} className="flex justify-start w-[452px] h-45 bg-linear-to-r from-[#ffffff33] to-[#3435411A] border-2 border-green rounded-3xl p-4 gap-4 shadow-xl shadow-green/20">
                     <Image
                         src={Menu.image}
                         alt={Menu.name}
@@ -41,11 +44,14 @@ export function ResultsBox() {
                         className="rounded-3xl"
                     />
                     <div className="flex flex-col gap-5">
-                        <p className="flex items-center justify-center w-18 h-12 text-white text-2xl font-bold  bg-[#7E5C4E]">
+                        <p className="flex items-center justify-center w-18 h-12 text-white text-2xl font-bold  bg-green rounded-xl">
                             {Menu.pass
                         }</p>
                         <p className="text-2xl font-bold">{Menu.name}</p>
-                        <p className="text-2xl font-bold">{Menu.price}</p>
+                        <div className="flex justify-center items-center gap-2"> 
+                            <p className="text-2xl text-red-500 font-bold">{Menu.price}</p>
+                            <p className="text-md font-bold text-gray-500">{Menu.price2}</p>
+                        </div>
                     </div>
                 </div>
             ))}
