474e402af413108db990717991c2fef5776c16bc
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-11-17 00:04:04 +0900

    create chatbox

--- frontend/src/app/answer/page.tsx
+++ frontend/src/app/answer/page.tsx
@@ -2,25 +2,15 @@
 
 import { Header } from "@/components/Header";
 import { ResultsBox } from "@/components/ResultsBox";
+import { ChatBox } from "@/components/ChatBox";
 
 export default function Answer() {
     return (
         <div className="GeneralPurposeBg min-h-screen ">
             <Header/>
-            <div className="flex items-center justify-center gap-4">
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <p className="text-3xl font-bold text-green">チャット内容</p>
-                    <div className="w-[617px] h-[580px] bg-white border-4 border-green rounded-3xl">
-
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <p className="text-3xl font-bold text-green">AIからの提案</p>
-                    <ResultsBox/>
-                    <ResultsBox/>
-                    <ResultsBox />
-                </div>
+            <div className="flex items-start justify-center gap-16">
+                <ChatBox />
+                <ResultsBox/>
             </div>
         </div>
     )
--- frontend/src/components/ResultsBox.tsx
+++ frontend/src/components/ResultsBox.tsx
@@ -1,20 +1,54 @@
 import Image from "next/image";
 
+type MenuProps = {
+    name: string
+    price: string
+    image: string
+}
+// 仮で入れてます
+const Menu =[
+    {
+    image:"/menuImg.png",
+    name:"ミラノ風ドリア",
+    price:"273円(税込300円)",
+    pass:"2101"
+    },
+    {
+    image: "/menuImg.png",
+    name: "海老グラタン",
+    price: "320円(税込352円)",
+    pass: "2102",
+  },
+  {
+    image: "/menuImg.png",
+    name: "チーズリゾット",
+    price: "280円(税込308円)",
+    pass: "2103",
+  },
+]
+
 export function ResultsBox() {
     return (
-        <div className="flex justify-start w-[452px] h-45 bg-linear-to-r from-[#ffffff33] to-[#3435411A] border-2 border-green rounded-3xl p-4 gap-4">
-            <Image
-                src="/menuImg.png"
-                alt="menu"
-                width={180}
-                height={150}
-                className="rounded-3xl"
-            />
-            <div className="flex flex-col gap-5">
-                <p className="flex items-center justify-center w-18 h-12 text-white text-2xl font-bold  bg-[#7E5C4E]">2101</p>
-                <p className="text-2xl font-bold">ミラノ風ドリア</p>
-                <p className="text-2xl font-bold">273円(税込300円)</p>
-            </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-3xl font-bold text-green">AIからの提案</p>
+            {Menu.map((Menu, index) => (
+                <div key={index} className="flex justify-start w-[452px] h-45 bg-linear-to-r from-[#ffffff33] to-[#3435411A] border-2 border-green rounded-3xl p-4 gap-4">
+                    <Image
+                        src={Menu.image}
+                        alt={Menu.name}
+                        width={180}
+                        height={150}
+                        className="rounded-3xl"
+                    />
+                    <div className="flex flex-col gap-5">
+                        <p className="flex items-center justify-center w-18 h-12 text-white text-2xl font-bold  bg-[#7E5C4E]">
+                            {Menu.pass
+                        }</p>
+                        <p className="text-2xl font-bold">{Menu.name}</p>
+                        <p className="text-2xl font-bold">{Menu.price}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
